722f970568a259452b9f71fa491c339e1d1aca6d
ArulmozhiMalvanan <arulmozhimalvannan@gmail.com>
2026-03-20 18:52:56 +0530

    Day 4

--- python-practice/threading_multithreading.py
+++ python-practice/threading_multithreading.py
@@ -1,5 +1,6 @@
 import threading
 import multiprocessing
+import time
 
 def task1():
     print("Task1 running")
@@ -15,27 +16,36 @@ t2.start()
 #real automation
 def test_login():
     print("login test")
+    time.sleep(2)
 
 def test_dashboard():
     print("Dasshboard test")
+    time.sleep(2)
+start = time.time()
+test_login()
+test_dashboard()
+end = time.time()
+print("Time:", end - start)
 
 t1 = threading.Thread(target=test_login)
 t2 = threading.Thread(target=test_dashboard)
-
+start = time.time()
 t1.start()
 t2.start()
+end = time.time()
+print("Time:", end - start)
 
 #Multiprocessing
 
 
-def task():
-    print("Running task")
+# def task():
+#     print("Running task")
 
-p1 = multiprocessing.Process(target=task)
-p2 = multiprocessing.Process(target=task)
+# p1 = multiprocessing.Process(target=task)
+# p2 = multiprocessing.Process(target=task)
 
-p1.start()
-p2.start()
+# p1.start()
+# p2.start()
 
 #Interview questions
 """
@@ -52,4 +62,18 @@ Thread --> UI, I/O bounds, Light weight,shared memory
 Multiprocessing -> CI/CD pipeline, testcase parllel run.
 
 """
+if __name__ == "__main__":
+    p1 = multiprocessing.Process(target=test_login)
+    p2 = multiprocessing.Process(target=test_dashboard)
 
+    start = time.time()
+
+    p1.start()
+    p2.start()
+
+    p1.join()
+    p2.join()
+
+    end = time.time()
+    print(end-start)
+    
